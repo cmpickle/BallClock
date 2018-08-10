3bb26782992f5d3ca24bd304eec207660c46cd14
Cameron Pickle <cmpickle@gmail.com>
2018-08-10 07:43:05 -0600

    Initial Commit

---
+++ cSharp/BallClock/BallClock/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallClock
+{
+    class Ball
+    {
+        public static int BallCount = 0;
+        public int Id { get; set; }
+
+        public Ball()
+        {
+            Id = ++BallCount;
+        }
+    }
+}
