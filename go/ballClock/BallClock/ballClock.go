745cd86dd15c54ce2195b3c7664f6c5e0691f911
Cameron Pickle <cmpickle@gmail.com>
2018-08-13 22:33:14 -0600

    implement BallClock

--- go/ballClock/BallClock/ballClock.go
+++ go/ballClock/BallClock/ballClock.go
@@ -16,3 +16,39 @@ type BallClock struct {
 func New(count int) *BallClock {
 	return &BallClock{count, stack.New(), stack.New(), stack.New(), queue.New()}
 }
+
+func (ballClock *BallClock) Tick() {
+	ball := ballClock.Main.Dequeue()
+	if ball == nil {
+		return
+	}
+
+	if ballClock.Min.Len() < 4 {
+		ballClock.Min.Push(ball)
+	} else {
+		ballClock.returnBalls(ballClock.Min)
+
+		if ballClock.FiveMin.Len() < 11 {
+			ballClock.FiveMin.Push(ball)
+		} else {
+			ballClock.returnBalls(ballClock.FiveMin)
+
+			if ballClock.Hour.Len() < 11 {
+				ballClock.Hour.Push(ball)
+			} else {
+				ballClock.returnBalls(ballClock.Hour)
+
+				ballClock.Main.Enqueue(ball)
+			}
+		}
+	}
+}
+
+func (ballClock *BallClock) returnBalls(stack *stack.Stack) {
+	for stack.Len() > 0 {
+		ball := stack.Pop()
+		if ball != nil {
+			ballClock.Main.Enqueue(ball)
+		}
+	}
+}
